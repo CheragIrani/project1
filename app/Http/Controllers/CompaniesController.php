be3d9cb9dd3c88140de8ca55b49c095200840076
cherag irani <cheragirani@cherags-MacBook-Pro.local>
2018-10-24 15:44:42 +0100

    My first Commit

---
+++ app/Http/Controllers/CompaniesController.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Companies;
+
+class CompaniesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $companies = Companies::paginate(10);
+        return view('index',compact('companies'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $company = new Companies();
+        $company->name = $request->get('name');
+        $company->save();
+ 
+    return redirect('companies')->with('success','Company has been added');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $company = Companies::find($id);
+        return view('edit',compact('companies','id'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $company= Employee::find($id);
+        $company->name=$request->get('name');
+        $company->save();
+        return redirect('companies');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $company = Companies::find($id);
+        $company->delete();
+        return redirect('companies')->with('success','Company Has Been Deleted');
+    }
+}
